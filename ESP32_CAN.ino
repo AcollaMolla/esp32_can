cbd81f20338dce969f641ca7a608a1a3a6e54327
Anton Engman <anton_engman@hotmail.com>
2021-07-24 17:22:09 +0200

    Fix typos

--- ESP32_CAN.ino
+++ ESP32_CAN.ino
@@ -1,13 +1,14 @@
 #include <ESP32CAN.h>
 #include <CAN_config.h>
+/*#include <Arduino_FreeRTOS.h>*/
 
 CAN_device_t CAN_cfg;
 
 void setup(){
   Serial.begin(115200);
-  CAN_cfg.speed CAN_SPEED_125KBPS;
-  CAN_cfg.tx_pin_id GPIO_NUM_5;
-  CAN_cfg.rx_pin_id GPIO_NUM_4;
+  CAN_cfg.speed = CAN_SPEED_125KBPS;
+  CAN_cfg.tx_pin_id = GPIO_NUM_5;
+  CAN_cfg.rx_pin_id = GPIO_NUM_4;
 
   CAN_cfg.rx_queue = xQueueCreate(10, sizeof(CAN_frame_t));
   ESP32Can.CANInit();
@@ -15,7 +16,7 @@ void setup(){
 
 void loop(){
   CAN_frame_t rx_frame;
-  if(xQueueReceive(CAN_cfg.rx_queue, rx_frame), 3 * portTICK_PERIOD_MS) == pdTRUE{
+  if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE){
     Serial.println("received data");
   }
 }
