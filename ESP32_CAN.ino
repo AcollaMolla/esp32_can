bdf04a4a7282da3122e9b094e2a70bf218b1d037
Anton Engman <anton_engman@hotmail.com>
2021-08-01 10:59:41 +0200

    Completely rewrote code to use low level driver instead

--- ESP32_CAN.ino
+++ ESP32_CAN.ino
@@ -1,26 +1,41 @@
-#include <ESP32CAN.h>
-#include <CAN_config.h>
-/*#include <Arduino_FreeRTOS.h>*/
-
-CAN_device_t CAN_cfg;
+#include <driver/can.h>
+#include <driver/gpio.h>
 
 void setup(){
   Serial.begin(115200);
-  Serial.println("Setup");
-  CAN_cfg.speed = CAN_SPEED_125KBPS;
-  CAN_cfg.tx_pin_id = GPIO_NUM_5;
-  CAN_cfg.rx_pin_id = GPIO_NUM_4;
+  setup_can_driver();
+}
 
-  CAN_cfg.rx_queue = xQueueCreate(10, sizeof(CAN_frame_t));
-  ESP32Can.CANInit();
+void setup_can_driver(){
+  can_general_config_t general_config={
+    .mode = CAN_MODE_ NORMAL,
+    .tx_io = (gpio_num_t)GPIO_NUM_5,
+    .rx_io = (gpio_num_t)GPIO_NUM_4,
+    .clkout_io = (gpio_num)CAN_IO_UNUSED,
+    .bus_off_io = (gpio_num)CAN_IO_UNUSED,
+    .tx_queue_len = 0,
+    .rx_queue_len = 65,
+    .alerts_enable = CAN_ALERT_ALL,
+    .clkout_divider = 0
+  };
+
+  can_timing_config_t timing_config = CAN_TIMING_CONFIG_125KBITS();
+  can_filter_config_t filter_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
+  esp_err_t error;
+  error = can_driver_install(&general_config, &timing_config, &filter_config);
+
+  if(error==ESP_OK){
+    Serial.println("Driver installed successfully");
+  }
+  else{
+    Serial.println("Driver installation failed...");
+    return;
+  }
 }
 
 void loop(){
-  CAN_frame_t rx_frame;
-  if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE){
-    Serial.println("received data");
-  }
-  else{
-    //Serial.println("no message");
+  can_message_t rx_frame;
+  if(can_receive(&rx_frame,pdMS_TO_TICKS(1000))==ESP_OK){
+    Serial.println("new msg");
   }
 }
