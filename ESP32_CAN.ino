c952b8543c780d9faf3453ba1414cf43e81310d9
Anton <antengman@gmail.com>
2021-08-29 17:58:59 +0200

    Got working can slave!

--- ESP32_CAN.ino
+++ ESP32_CAN.ino
@@ -3,19 +3,20 @@
 
 void setup(){
   Serial.begin(115200);
+  Serial.println("setup");
   setup_can_driver();
 }
 
 void setup_can_driver(){
   can_general_config_t general_config={
-    .mode = CAN_MODE_ NORMAL,
+    .mode = CAN_MODE_LISTEN_ONLY,
     .tx_io = (gpio_num_t)GPIO_NUM_5,
     .rx_io = (gpio_num_t)GPIO_NUM_4,
-    .clkout_io = (gpio_num)CAN_IO_UNUSED,
-    .bus_off_io = (gpio_num)CAN_IO_UNUSED,
+    .clkout_io = (gpio_num_t)CAN_IO_UNUSED,
+    .bus_off_io = (gpio_num_t)CAN_IO_UNUSED,
     .tx_queue_len = 0,
     .rx_queue_len = 65,
-    .alerts_enable = CAN_ALERT_ALL,
+    .alerts_enabled = CAN_ALERT_ALL,
     .clkout_divider = 0
   };
 
@@ -31,11 +32,28 @@ void setup_can_driver(){
     Serial.println("Driver installation failed...");
     return;
   }
+  if(can_start() == ESP_OK){
+    Serial.println("started successfully!");
+  }
+  else{
+    Serial.println("cant start driver...");
+  }
 }
 
 void loop(){
   can_message_t rx_frame;
+  uint32_t alerts;
   if(can_receive(&rx_frame,pdMS_TO_TICKS(1000))==ESP_OK){
-    Serial.println("new msg");
+    Serial.print(rx_frame.identifier);
+    Serial.print(": ");
+    if(!(rx_frame.flags & CAN_MSG_FLAG_RTR)){
+      for(int i=0;i<rx_frame.data_length_code;i++){
+        Serial.print(rx_frame.data[i]);
+        Serial.print(",");
+      }
+      Serial.println();
+    }
+  }else{
+    Serial.print(can_read_alerts(&alerts, pdMS_TO_TICKS(1000)));
   }
 }
